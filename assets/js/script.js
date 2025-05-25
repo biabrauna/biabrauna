c3094442f3edb6b165c54236575852a21716e026
biabrauna <biabn443@gmail.com>
2025-05-25 17:54:33 -0300

    feat: adding i18n to switch language

--- assets/js/script.js
+++ assets/js/script.js
@@ -310,13 +310,13 @@ function setLanguage(lang) {
     const key = el.getAttribute('data-i18n');
     if (translations[lang][key]) {
       if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA' || el.hasAttribute('data-i18n-placeholder')) {
-        // handled below
+        // el.placeholder = translations[lang][key];
       } else {
         el.innerHTML = translations[lang][key];
       }
     }
   });
-  // Placeholders
+//   Placeholders
   document.querySelectorAll('[data-i18n-placeholder]').forEach(el => {
     const key = el.getAttribute('data-i18n-placeholder');
     if (translations[lang][key]) {
